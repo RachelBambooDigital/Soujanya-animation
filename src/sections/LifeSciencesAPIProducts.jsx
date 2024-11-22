e3eea4e68d89c06f70f27de4aeb44d2eb060a131
ServerBamboo <server@bamboodigital.in>
2024-11-22 19:47:29 +0530

    shopify

--- src/sections/LifeSciencesAPIProducts.jsx
+++ src/sections/LifeSciencesAPIProducts.jsx
@@ -7,7 +7,19 @@ const LifeSciencesAPIProducts = ({
   product1Empirical,
   product1Cas,
   product1Molecular,
-  product1Desc
+  product1Desc,
+
+  product2Title,
+  product2Empirical,
+  product2Cas,
+  product2Molecular,
+  product2Desc,
+
+  product3Title,
+  product3Empirical,
+  product3Cas,
+  product3Molecular,
+  product3Desc,
 }) => {
   return (
     <div className="w-full bg-white">
@@ -34,18 +46,18 @@ const LifeSciencesAPIProducts = ({
             MolecularWeight={product1Molecular} // Correctly passing string
           />
           <LifeSciencesAPIProductsCard
-            title={product1Title} // Correctly passing string
-            description={product1Desc} // Correctly passing string
-            EmpiricalFormula={product1Empirical} // Correctly passing string
-            CASNumber={product1Cas} // Correctly passing string
-            MolecularWeight={product1Molecular} // Correctly passing string
+            title={product2Title} // Correctly passing string
+            description={product2Desc} // Correctly passing string
+            EmpiricalFormula={product2Empirical} // Correctly passing string
+            CASNumber={product2Cas} // Correctly passing string
+            MolecularWeight={product2Molecular} // Correctly passing string
           />
           <LifeSciencesAPIProductsCard
-            title={product1Title} // Correctly passing string
-            description={product1Desc} // Correctly passing string
-            EmpiricalFormula={product1Empirical} // Correctly passing string
-            CASNumber={product1Cas} // Correctly passing string
-            MolecularWeight={product1Molecular} // Correctly passing string
+            title={product3Title} // Correctly passing string
+            description={product3Desc} // Correctly passing string
+            EmpiricalFormula={product3Empirical} // Correctly passing string
+            CASNumber={product3Cas} // Correctly passing string
+            MolecularWeight={product3Molecular} // Correctly passing string
           />
           {/* Add more product cards if necessary */}
         </div>
